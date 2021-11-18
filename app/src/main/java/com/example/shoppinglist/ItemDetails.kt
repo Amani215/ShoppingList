88dd73bda0d38d39096d7c62254f76aa77235b51
Amani215 <amanibrik5@gmail.com>
2021-11-18 21:14:03 +0100

    Added: Retrofit

--- app/src/main/java/com/example/shoppinglist/ItemDetails.kt
+++ app/src/main/java/com/example/shoppinglist/ItemDetails.kt
@@ -6,6 +6,14 @@ import android.os.Bundle
 import com.example.shoppinglist.data.AppDatabase
 import com.example.shoppinglist.data.Item
 import com.example.shoppinglist.databinding.ItemDetailsBinding
+import com.example.shoppinglist.retrofitConversion.MoneyAPI
+import com.example.shoppinglist.retrofitConversion.MoneyResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class ItemDetails : AppCompatActivity() {
     private lateinit var  binding: ItemDetailsBinding
@@ -17,11 +25,52 @@ class ItemDetails : AppCompatActivity() {
         setContentView(binding.root)
 
         var item: Item = intent.getSerializableExtra("itemId") as Item
+        var itemPrice: Int = item.estimatedPrice
 
         binding.tvNameDetail.setText(item.name)
         binding.tvDescriptionDetail.setText(item.description)
         binding.tvCategoryDetail.setText(item.category.toString())
         binding.tvStatusDetail.setText(item.status.toString())
-        binding.tvPriceDetail.setText(item.estimatedPrice.toString())
+        binding.tvPriceDetail.setText(itemPrice.toString()+" HUF")
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.frankfurter.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val currencyAPI = retrofit.create(MoneyAPI::class.java)
+
+        binding.btnGetRates.setOnClickListener {
+            binding.tvResult.setText("")
+            binding.tvPriceDetail.setText(itemPrice.toString()+" HUF")
+
+            val rateCall = currencyAPI.getRates("HUF")
+
+            rateCall.enqueue(object : Callback<MoneyResult> {
+                override fun onFailure(call: Call<MoneyResult>, t: Throwable) {
+                    binding.tvResult.setText(t.message)
+                }
+
+                override fun onResponse(call: Call<MoneyResult>, response: Response<MoneyResult>) {
+                    var cadRate: Double? = response.body()?.rates?.CAD?.toDouble()
+                    var priceInCAD = itemPrice * cadRate!!
+
+                    binding.tvPriceDetail.append("\n"+priceInCAD.toString()+" CAD")
+                    binding.tvResult.append("CAD: "+cadRate.toString()+"\n")
+
+                    var eurRate: Double? = response.body()?.rates?.EUR?.toDouble()
+                    var priceInEUR = itemPrice * eurRate!!
+
+                    binding.tvPriceDetail.append("\n"+priceInEUR.toString()+" EUR")
+                    binding.tvResult.append("EUR: "+eurRate.toString()+"\n")
+
+                    var jpyRate: Double? = response.body()?.rates?.JPY?.toDouble()
+                    var priceInJPY = itemPrice * jpyRate!!
+
+                    binding.tvPriceDetail.append("\n"+priceInJPY.toString()+" JPY")
+                    binding.tvResult.append("JPY: "+jpyRate.toString()+"\n")
+                }
+            })
+        }
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/retrofitConversion/MoneyAPI.kt
@@ -0,0 +1,11 @@
+package com.example.shoppinglist.retrofitConversion
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MoneyAPI {
+    @GET("/latest")
+    fun getRates(@Query("from") base: String): Call<MoneyResult>
+}
